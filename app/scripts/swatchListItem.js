4f024cdf9dbb8b29d4a8e8c73b670e8d151964ba
Sarah Vessels <cheshire137@gmail.com>
2015-07-03 14:45:32 -0400

    Use tinycolor to determine dark/light
    Show hex in each color swatch.

--- app/scripts/swatchListItem.js
+++ app/scripts/swatchListItem.js
@@ -1,13 +1,17 @@
 'use strict';
-var React = require('react');
+var React = require('react'),
+    tinycolor = require('tinycolor2');
 var SwatchListItem = React.createClass({
   render: function() {
     var hex = this.props.swatch.hex;
-    var className = 'swatch swatch-' + this.props.swatch.name;
+    var className = 'swatch swatch-' + this.props.swatch.name +
+                    (tinycolor(hex).isDark() ? ' dark' : ' light');
     var style = {backgroundColor: hex};
     return (
       <li className="swatch-list-item">
-        <div className={className} title={hex} style={style}></div>
+        <div className={className} title={hex} style={style}>
+          {{hex}}
+        </div>
       </li>
     );
   }
