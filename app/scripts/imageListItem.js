237758f4e37cc7ffbc7acc2fb575d3977f064bcb
Sarah Vessels <cheshire137@gmail.com>
2015-07-09 18:37:04 -0400

    Add shuffle function

--- app/scripts/imageListItem.js
+++ app/scripts/imageListItem.js
@@ -5,7 +5,7 @@ var React = require('react'),
     tinycolor = require('tinycolor2');
 var ImageListItem = React.createClass({
   getInitialState: function() {
-    return {swatches: []};
+    return {swatches: [], hexIndex: 0};
   },
   onImageLoad: function(e) {
     var img = $(e.target);
@@ -40,39 +40,47 @@ var ImageListItem = React.createClass({
     }
     return text;
   },
-  previewSwatches: function(e) {
-    e.preventDefault();
-    var link = $(e.target);
-    link.blur();
-
+  getHexes: function() {
+    return this.state.swatches.map(function(swatch) { return swatch.hex; });
+  },
+  getCssColors: function(i) {
+    var numSwatches = this.state.swatches.length;
+    var hexes = this.getHexes();
     var defaultDark = '#333';
     var defaultLight = '#F0F0F0';
-    var numSwatches = this.state.swatches.length;
-    var hexes = this.state.swatches.map(function(swatch) { return swatch.hex; });
+    if (typeof i === 'undefined') {
+      i = this.state.hexIndex;
+    }
 
-    var bodyBg = hexes[1] || hexes[4] || hexes[3] || hexes[2] || hexes[0];
-    var bodyText = hexes[2];
+    var hex0 = hexes[i];
+    var hex1 = hexes[(i + 1) % numSwatches];
+    var hex2 = hexes[(i + 2) % numSwatches];
+    var hex3 = hexes[(i + 3) % numSwatches];
+    var hex4 = hexes[(i + 4) % numSwatches];
+
+    var bodyBg = hex1 || hex4 || hex3 || hex2 || hex0;
+    var bodyText = hex2;
     if (!bodyText) {
       bodyText = tinycolor(bodyBg).isDark() ? defaultLight : defaultDark;
     }
     bodyText = this.getReadableText(bodyBg, bodyText);
 
-    var bodyLink = hexes[3] || hexes[0] || hexes[1] || hexes[4] || hexes[2];
+    var bodyLink = hex3 || hex0 || hex1 || hex4 || hex2;
     bodyLink = this.getReadableText(bodyBg, bodyLink);
 
-    var dropdownBg = hexes[3] || hexes[1] || hexes[0] || hexes[4] || hexes[2];
-    var dropdownLink = hexes[4] || hexes[0] || hexes[2] || hexes[3] || hexes[1];
+    var dropdownBg = hex3 || hex1 || hex0 || hex4 || hex2;
+    var dropdownLink = hex4 || hex0 || hex2 || hex3 || hex1;
     dropdownLink = this.getReadableText(dropdownBg, dropdownLink);
 
-    var headerBg = hexes[0] || hexes[3] || hexes[4] || hexes[1];
-    var headerText = hexes[2];
+    var headerBg = hex0 || hex3 || hex4 || hex1;
+    var headerText = hex2;
     if (!headerText) {
       headerText = tinycolor(headerBg).isDark() ? defaultLight : defaultDark;
     }
     headerText = this.getReadableText(headerBg, headerText, {size: 'large'});
 
-    var footerBg = hexes[4] || hexes[3] || hexes[2] || hexes[1] || hexes[0];
-    var footerLink = hexes[1] || hexes[0] || hexes[2] || hexes[3];
+    var footerBg = hex4 || hex3 || hex2 || hex1 || hex0;
+    var footerLink = hex1 || hex0 || hex2 || hex3;
     var isFooterDark = tinycolor(footerBg).isDark();
     if (!footerLink) {
       footerLink = isFooterDark ? defaultLight : defaultDark;
@@ -87,16 +95,30 @@ var ImageListItem = React.createClass({
     }
     footerText = this.getReadableText(footerBg, footerText);
 
-    this.applyCssColors({footerLink: footerLink, footerText: footerText,
+    return {footerLink: footerLink, footerText: footerText,
                          footerBg: footerBg, headerText: headerText,
                          headerBg: headerBg, dropdownBg: dropdownBg,
                          dropdownLink: dropdownLink, bodyLink: bodyLink,
-                         bodyText: bodyText, bodyBg: bodyBg});
-
+                         bodyText: bodyText, bodyBg: bodyBg};
+  },
+  previewSwatches: function(e) {
+    e.preventDefault();
+    var link = $(e.target);
+    link.blur();
+    this.applyCssColors(this.getCssColors());
     // var listItem = link.closest('.image-list-item');
     // var fromTop = listItem.offset().top - $('.nav-wrapper').outerHeight() -
     //               $('.page-footer').outerHeight() - 32;
     // $('html, body').animate({scrollTop: fromTop}, 750);
+  },
+  shuffleSwatches: function(e) {
+    e.preventDefault();
+    var link = $(e.target);
+    link.blur();
+    var numSwatches = this.state.swatches.length;
+    var newIndex = (this.state.hexIndex + 1) % numSwatches;
+    this.setState({hexIndex: newIndex});
+    this.applyCssColors(this.getCssColors(newIndex));
   },
   applyCssColors: function(colors) {
     $('body').css({'background-color': colors.bodyBg, color: colors.bodyText});
@@ -135,6 +157,7 @@ var ImageListItem = React.createClass({
                            this.state.swatches.map(function(swatch) {
                              return swatch.hex.replace(/^#/, '');
                            }).join(',');
+    var shuffleStyle = {display: this.props.isActive ? 'inline-block' : 'none'};
     return (
       <li className="image-list-item clearfix">
         <a href={this.props.image.link} target="_blank" className="image-link">
@@ -155,6 +178,12 @@ var ImageListItem = React.createClass({
                 <a href="#" onClick={this.previewSwatches}>
                   <i className="mdi-action-visibility"></i>
                   Preview
+                </a>
+              </li>
+              <li style={shuffleStyle}>
+                <a href="#" onClick={this.shuffleSwatches}>
+                  <i className="mdi-av-shuffle"></i>
+                  Shuffle
                 </a>
               </li>
             </ul>
